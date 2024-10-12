da9e7593d525a62c07eead001679a9e9fcd17571
unknown <polibos6366@gmail.com>
2024-10-11 19:43:58 -0600

    any type

--- index.ts
+++ index.ts
@@ -55,13 +55,23 @@
 //   name: "Mark",
 // };
 
-/**
- * Assign the correct types to each of the following variables
- */
-let message: string = "Hello, TypeScript!";
-let age: number = 42;
-let isStudent: boolean = true;
-let fetched: null = null;
-let user: undefined = undefined;
-let largeNumber: bigint = 9007199254740991n;
-let unique: symbol = Symbol("uniqueSymbol");
+// /**
+//  * Assign the correct types to each of the following variables
+//  */
+// let message: string = "Hello, TypeScript!";
+// let age: number = 42;
+// let isStudent: boolean = true;
+// let fetched: null = null;
+// let user: undefined = undefined;
+// let largeNumber: bigint = 9007199254740991n;
+// let unique: symbol = Symbol("uniqueSymbol");
+
+// // any Type (avoid using it)
+// let firstName: any = "Mark";
+
+// firstName = 123;
+// firstName = [];
+
+// function returnParam (param)  {
+//     return param
+// }
