7f540f1d51219632cab099ad4239a94809ccc05a
unknown <polibos6366@gmail.com>
2024-10-11 18:40:50 -0600

    How TypeScript Helps You Catch Errors

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello world");
+const students = [
+    {
+        name: "John",
+        score: "70",
+    },
+    {
+        name: "Mark",
+        score: "90",
+    },
+];
+for (const student of students) {
+    console.log("Name:", student.name);
+    console.log("Score:", student.score);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const students = [
+  {
+    name: "John",
+    score: "70",
+  },
+  {
+    name: "Mark",
+    score: "90",
+  },
+];
+
+for (const student of students) {
+  console.log("Name:", student.name);
+  console.log("Score:", student.score);
+}
