e7fab3727d53e3402de77da3679e27e447ea8b99
unknown <polibos6366@gmail.com>
2024-10-11 19:11:27 -0600

    string, number and boolean

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 "use strict";
-function addNumber(a, b) {
-    return a + b;
-}
-console.log(addNumber(2, 3));
-// console.log(addNumber("text", 3));
+// Primitive types
+// Strings // manually typed
+let firstName = "John";
+//inference by typescript
+let automobile = "BMW";
+// literal value
+const city = "New york";
+let students = 32;
+let studentAsString = students.toString();
+// Numbers
+let age = 32;
+let year = 2024;
+// literal value
+const numberOfMember = 61;
+let stringToNumber = parseInt("1985");
+//Booleans true or false
+let isStudent = true;
+// literal value
+const alwaysStudent = true;
+let minimumAge = age >= 6 ? true : false;
--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
-function addNumber(a: number, b: number) {
-  return a + b;
-}
+// Primitive types
+// Strings // manually typed
+let firstName: string = "John";
 
-console.log(addNumber(2, 3));
-// console.log(addNumber("text", 3));
+//inference by typescript
+let automobile = "BMW";
+
+// literal value
+const city = "New york";
+
+let students = 32;
+let studentAsString: string = students.toString();
+
+// Numbers
+let age: number = 32;
+let year: number = 2024;
+// literal value
+const numberOfMember = 61;
+let stringToNumber: number = parseInt("1985");
+
+//Booleans true or false
+let isStudent: boolean = true;
+// literal value
+const alwaysStudent = true;
+let minimumAge: boolean = age >= 6 ? true : false;
