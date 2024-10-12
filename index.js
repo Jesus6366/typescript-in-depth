e2e73c33a2f936d717458ce043fc7dc54b78670f
unknown <polibos6366@gmail.com>
2024-10-11 19:15:47 -0600

    null and undefined Types

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 "use strict";
-// Primitive types
-// Strings // manually typed
-let firstName = "John";
-//inference by typescript
-let automobile = "BMW";
-// literal value
-const city = "New york";
-let students = 32;
-let studentAsString = students.toString();
-// Numbers
-let age = 32;
-let year = 2024;
-// literal value
-const numberOfMember = 61;
-let stringToNumber = parseInt("1985");
-//Booleans true or false
-let isStudent = true;
-// literal value
-const alwaysStudent = true;
-let minimumAge = age >= 6 ? true : false;
+// // Primitive types
+// // Strings // manually typed
+// let firstName: string = "John";
+// //inference by typescript
+// let automobile = "BMW";
+// // literal value
+// const city = "New york";
+// let students = 32;
+// let studentAsString: string = students.toString();
+// // Numbers
+// let age: number = 32;
+// let year: number = 2024;
+// // literal value
+// const numberOfMember = 61;
+// let stringToNumber: number = parseInt("1985");
+// //Booleans true or false
+// let isStudent: boolean = true;
+// // literal value
+// const alwaysStudent = true;
+// let minimumAge: boolean = age >= 6 ? true : false;
+// null and undefined types
+let user;
+console.log(user);
+let userRole;
+console.log((userRole = null));
--- index.ts
+++ index.ts
@@ -1,25 +1,32 @@
-// Primitive types
-// Strings // manually typed
-let firstName: string = "John";
+// // Primitive types
+// // Strings // manually typed
+// let firstName: string = "John";
 
-//inference by typescript
-let automobile = "BMW";
+// //inference by typescript
+// let automobile = "BMW";
 
-// literal value
-const city = "New york";
+// // literal value
+// const city = "New york";
 
-let students = 32;
-let studentAsString: string = students.toString();
+// let students = 32;
+// let studentAsString: string = students.toString();
 
-// Numbers
-let age: number = 32;
-let year: number = 2024;
-// literal value
-const numberOfMember = 61;
-let stringToNumber: number = parseInt("1985");
+// // Numbers
+// let age: number = 32;
+// let year: number = 2024;
+// // literal value
+// const numberOfMember = 61;
+// let stringToNumber: number = parseInt("1985");
 
-//Booleans true or false
-let isStudent: boolean = true;
-// literal value
-const alwaysStudent = true;
-let minimumAge: boolean = age >= 6 ? true : false;
+// //Booleans true or false
+// let isStudent: boolean = true;
+// // literal value
+// const alwaysStudent = true;
+// let minimumAge: boolean = age >= 6 ? true : false;
+
+// null and undefined types
+let user: undefined;
+console.log(user);
+
+let userRole: null;
+console.log((userRole = null));
